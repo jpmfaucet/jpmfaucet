613a6798184732c6e8eb8868656cb31897080328
JPMCoin <47649452+JPMCoin@users.noreply.github.com>
2019-02-14 22:37:04 -0500

    Update JPMCoin.sol

--- JPMCoin/JPMCoin.sol
+++ JPMCoin/JPMCoin.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.25;
 
 /*
-Copyright (c) 2019 JPMorgan
+Copyright (c) 2019 JPM Faucet
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
